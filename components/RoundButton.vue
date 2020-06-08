d7c49d15233e944f5056c41904e8e502c6ad12ad
Kota Yatagai <kouta.giants0706@gmail.com>
2020-06-08 00:12:10 +0000

    close #5

---
+++ components/RoundButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <a :href="url">
+    <button class="RoundButton">{{ name }}</button>
+  </a>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['name', 'url']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/variables.scss';
+.RoundButton {
+  background: $blue;
+  color: $white;
+  border-radius: 24px;
+  width: 200px;
+  height: 48px;
+  transition: 0.3s;
+  font-family: Noto Sans CJK JP;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 180%;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.03em;
+}
+.RoundButton:hover {
+  opacity: 0.5;
+  transition: 0.3s;
+}
+.RoundButton:focus {
+  outline: none;
+}
+</style>
