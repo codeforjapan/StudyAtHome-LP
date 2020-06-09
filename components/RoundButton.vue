8229f29189468964fece958a5198084873e802f1
Kota Yatagai <kouta.giants0706@gmail.com>
2020-06-09 14:51:32 +0000

    Update RoundButton

--- components/RoundButton.vue
+++ components/RoundButton.vue
@@ -1,32 +1,34 @@
 <template>
   <a :href="url">
-    <button class="RoundButton">{{ name }}</button>
+    <v-btn rounded>{{ name }}</v-btn>
   </a>
 </template>
 
 <script>
 export default {
-  // eslint-disable-next-line vue/require-prop-types
-  props: ['name', 'url']
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.RoundButton {
-  background: $base-blue;
+.v-btn {
+  background-color: $base-blue !important;
   color: $white;
-  border-radius: 24px;
   width: 200px;
   height: 48px;
   transition: 0.3s;
   font-family: Noto Sans CJK JP;
-  font-style: normal;
   font-weight: bold;
   font-size: 15px;
-  line-height: 180%;
-  align-items: center;
-  text-align: center;
-  letter-spacing: 0.03em;
 }
 .RoundButton:hover {
   opacity: 0.5;
