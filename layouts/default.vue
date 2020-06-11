9275a1706ddd502c40817c8af77ff2a22659126a
Kota Yatagai <kouta.giants0706@gmail.com>
2020-06-11 09:26:29 +0000

    remake removed pages

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-app-bar app>
+      <v-toolbar-title>
+        おうちで時間割
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
