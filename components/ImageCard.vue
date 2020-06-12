3c2f7c559638c59e523340ee0ebe7c79153935b6
Kota Yatagai <kouta.giants0706@gmail.com>
2020-06-12 11:38:25 +0900

    画像ボタンの作成 (#13)
    * 画像ボタンの作成
    
    * Update components/ImageCard.vue
    
    Co-authored-by: Yuto Ashida <34832037+y-chan@users.noreply.github.com>
    
    * Changed props name to camel case
    
    * remake removed pages
    
    * Fix unexpected commit
    
    * Update components/ImageCard.vue
    
    Co-authored-by: Yuto Ashida <34832037+y-chan@users.noreply.github.com>
    
    Co-authored-by: Yuto Ashida <34832037+y-chan@users.noreply.github.com>

---
+++ components/ImageCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+    outlined
+    :href="url"
+    class="ImageCard"
+  >
+    <div class="ImageContent">
+      <v-img :src="imgPath"></v-img>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    imgPath: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ImageCard {
+  width: 340px;
+  height: 80px;
+  margin: 0 auto;
+  text-align: center;
+}
+.ImageCard .ImageContent {
+  width: 150px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+}
+</style>
