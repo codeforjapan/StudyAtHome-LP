e095bfc03ff07ee4dde122a7fac1b28c681d973b
Kota Yatagai <kouta.giants0706@gmail.com>
2020-06-12 06:01:00 +0000

    コントリビュータのアイコンと名前、役割の表示

---
+++ components/ContributorCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card flat class="ContriCard">
+    <v-avatar size="100px">
+      <img :src="iconPath" />
+    </v-avatar>
+    <span class="ContriText ContriName">{{ name }}</span>
+    <span class="ContriText ContriRole">{{ role }}</span>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    iconPath: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ContriCard {
+  display: inline-block;
+  width: 100px;
+  height: 145px;
+}
+.ContriCard img {
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1px solid #f3f3f4;
+  box-sizing: border-box;
+}
+.ContriText {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  letter-spacing: 0.03em;
+  line-height: 180%;
+}
+.ContriName {
+  margin-top: 4px;
+  font-weight: bold;
+  font-size: 15px;
+}
+.ContriRole {
+  font-weight: normal;
+  font-size: 11px;
+}
+</style>
