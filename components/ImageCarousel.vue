178efe58fd72c078c697f1b07084ed4f9decf076
Kazuki Imamura <kaizumaki@gmail.com>
2020-06-13 21:37:17 +0900

    カルーセルのコンポーネントを作成

---
+++ components/ImageCarousel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div ref="carouselOuter" class="ImageCarousel-Outer">
+    <div ref="carousel" class="ImageCarousel" :style="`width: calc(100% + ${carouselSlideWidth / 2}px)`">
+      <carousel
+        :scrollPerPage="false"
+        :navigationEnabled="true"
+        :navigationNextLabel="'next →'"
+        :navigationPrevLabel="'← prev'"
+        :paginationEnabled="false"
+        :perPageCustom="[[600, 3], [768, 5]]"
+      >
+        <slide
+          v-for="(item, index) in items"
+          :key="index"
+          class="ImageCarousel-Slide"
+        >
+          <img ref="carouselImg" :src="item.filePath" alt="" />
+        </slide>
+        <slide class="ImageCarousel-Slide">
+          <div :style="`width: ${carouselSlideWidth}px; height: ${carouselSlideHeight}px;`"></div>
+        </slide>
+      </carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { filePath: '/index.jpg' },
+        { filePath: '/page-2.jpg' },
+        { filePath: '/page-2.jpg' },
+        { filePath: '/page-2.jpg' },
+        { filePath: '/page-2.jpg' },
+        { filePath: '/page-2.jpg' },
+        { filePath: '/page-2.jpg' }
+      ],
+      carouselSlideWidth: null,
+      carouselSlideHeight: null,
+      carouselSlideImgHeight: null
+    }
+  },
+  methods: {
+    handleSlideStyle: function () {
+      const carouselSlide = document.getElementsByClassName('VueCarousel-slide')[0]
+      const carouselImg = this.$refs.carouselImg
+      this.carouselSlideWidth = carouselSlide.clientWidth
+      this.carouselSlideHeight = carouselSlide.clientHeight
+      this.carouselSlideImgHeight = carouselImg.clientHeight
+    },
+    handleNavigationWidth: function () {
+      const carouselOuter = this.$refs.carouselOuter
+      const carouselInner = document.getElementsByClassName('VueCarousel-inner')[0]
+      const navigation = document.getElementsByClassName('VueCarousel-navigation')[0]
+      navigation.style.width = `${carouselOuter.clientWidth}px`
+      carouselInner.style.height = 'initial'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.handleSlideStyle()
+      setTimeout(() => {
+        this.handleNavigationWidth()
+      })
+    })
+    window.addEventListener('resize', this.handleSlideStyle)
+    window.addEventListener('resize', this.handleNavigationWidth)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleSlideStyle)
+    window.removeEventListener('resize', this.handleNavigationWidth)
+  }
+}
+</script>
+
+<style lang="scss">
+.ImageCarousel-Outer {
+  overflow: hidden;
+}
+.ImageCarousel-Slide {
+  padding-right: 8px;
+}
+.VueCarousel-navigation {
+  display: flex;
+  justify-content: space-between;
+}
+.VueCarousel-navigation-button {
+  position: relative !important;
+  top: 0 !important;
+  transform: none !important;
+}
+</style>
