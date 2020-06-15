6a0dc56897981acf47d4ad7d6e26ce71192fb84a
y-chan <y_chan_crypto@yahoo.co.jp>
2020-06-16 08:29:45 +0900

    remove px

--- components/StartButton.vue
+++ components/StartButton.vue
@@ -28,6 +28,6 @@ export default {
   letter-spacing: 0.03em;
   font-size: 20px;
   line-height: 180%;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
 }
 </style>
