497824fe84a021e51aac47e85fd8fd7031124cc6
MaySoMusician <maysomusician@gmail.com>
2020-11-28 16:36:09 +0900

    Adapt news list page to @nuxt/content

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,93 +1,60 @@
 <template>
-  <v-layout column justify-center align-center>
-    <h1 class="title text-center justify-center">
-      <span class="titleEn">News</span>
-      <span class="titleJa">最新情報</span>
-    </h1>
-    <v-row justify="center">
-      <v-col v-for="(item, i) in items" :key="i" cols="12" sm="6">
-        <article>
-          <v-card elevation="0">
-            <v-img :src="item.thumbnail.url" alt="" />
-            <v-card-subtitle class="cardSubtitle">
-              <nuxt-link class="cardUrl" :to="'news/' + item.id">
-                <time :datetime="item.createdAt">{{
-                  formatDate(item.createdAt)
-                }}</time>
-              </nuxt-link>
-            </v-card-subtitle>
-            <v-card-title class="cardTitle">
-              <nuxt-link class="cardUrl" :to="'news/' + item.id">
-                <h2>{{ item.title }}</h2>
-              </nuxt-link>
-            </v-card-title>
-          </v-card>
-        </article>
-      </v-col>
-    </v-row>
-  </v-layout>
+  <v-row class="NewsList-Container" justify="center" align="center">
+    <v-col cols="12">
+      <h1 class="NewsList-Title-Wrapper text-center justify-center">
+        <span class="NewsList-Title-English">News</span>
+        <span class="NewsList-Title-Japanese">最新情報</span>
+      </h1>
+    </v-col>
+    <v-col v-for="(item, i) in newsArticles" :key="i" cols="12" lg="4" sm="6">
+      <v-card tag="article" elevation="0" :ripple="false" nuxt :to="item.path">
+        <v-img
+          :src="`/news/${item.thumbnailFilename}`"
+          alt=""
+          :aspect-ratio="340 / 200"
+          contain
+        />
+        <v-card-subtitle class="NewsList-Card-DateTime">
+          <time :datetime="item.createdAt">{{
+            formatDate(item.createdAt)
+          }}</time>
+        </v-card-subtitle>
+        <v-card-title class="NewsList-Card-Title">
+          <h2>{{ item.title }}</h2>
+        </v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import axios from 'axios'
 import dayjs from 'dayjs'
+import { IContentDocument } from '@nuxt/content/types/content'
 
-type DataType = {
-  items: NewsType[]
+type Data = {
+  newsArticles: IContentDocument[]
 }
 
-type NewsListType = {
-  contents: NewsType[]
-  totalCount: number
-  offset: number
-  limit: number
+type Methods = {
+  formatDate(date: Date): string
 }
 
-type NewsType = {
-  id: string
-  createdAt: string
-  updatedAt: string
-  title: string
-  thumbnail: ThumbnailType
-  content: string
-}
+export default Vue.extend<Data, Methods, unknown, unknown>({
+  async asyncData({ $content }) {
+    const articles = await $content('news').fetch()
 
-type ThumbnailType = {
-  url: string
-}
-
-export default Vue.extend({
-  name: 'Index',
-  async asyncData() {
-    const { data } = await axios.get<NewsListType>(
-      'https://studyathome.microcms.io/api/v1/news',
-      {
-        headers: { 'X-API-KEY': process.env.API_KEY },
-      }
-    )
     return {
-      items: data.contents,
+      newsArticles: Array.isArray(articles) ? articles : [articles],
     }
   },
-  data(): DataType {
+  data() {
     return {
-      items: [
-        {
-          id: '',
-          createdAt: '',
-          updatedAt: '',
-          title: '',
-          thumbnail: {
-            url: '',
-          },
-          content: '',
-        },
-      ],
+      newsArticles: [],
     }
   },
   methods: {
-    formatDate(date: string): string {
+    formatDate(date: Date): string {
       return dayjs(date).format('YYYY.MM.DD')
     },
   },
@@ -95,42 +62,65 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.title {
-  margin: 17px auto;
-  font-weight: bold;
-  padding: 2px 0;
-  letter-spacing: 0.03em;
-  &En {
-    color: #0071c2;
-    display: block;
-    font-size: 17px;
+.NewsList {
+  &-Container {
+    max-width: 1000px;
+    margin: {
+      left: auto;
+      right: auto;
+    }
   }
-  &Ja {
-    font-size: 24px;
-    display: block;
-    color: #424242;
-  }
-}
-.cardUrl {
-  text-decoration: none;
-  color: black;
-}
-.cardTitle {
-  padding: 0 6px 4px 6px;
-  letter-spacing: 0.03em;
-  color: #424242;
 
-  h2 {
-    font-weight: bold;
-    font-size: 17px;
-    line-height: 180%;
+  &-Title {
+    &-Wrapper {
+      margin: 17px auto;
+      font-weight: bold;
+      padding: 2px 0;
+      letter-spacing: 0.03em;
+    }
+    &-English {
+      color: #0071c2;
+      display: block;
+      font-size: 17px;
+    }
+    &-Japanese {
+      font-size: 24px;
+      display: block;
+      color: #424242;
+    }
   }
-}
-.cardSubtitle {
-  font-weight: bold;
-  font-size: 12px;
-  padding: 8px 6px 2px 6px;
-  letter-spacing: 0.03em;
-  color: #424242;
+
+  &-Card {
+    &-DateTime {
+      font-weight: bold;
+      font-size: 12px;
+      letter-spacing: 0.03em;
+      color: #424242;
+      padding: {
+        top: 8px;
+        right: 6px;
+        bottom: 2px;
+        left: 6px;
+      }
+    }
+
+    &-Title {
+      letter-spacing: 0.03em;
+      color: #424242;
+
+      padding: {
+        top: 0;
+        right: 6px;
+        bottom: 4px;
+        left: 6px;
+      }
+
+      h2 {
+        font-weight: bold;
+        font-size: 17px;
+        line-height: 180%;
+      }
+    }
+  }
 }
 </style>
